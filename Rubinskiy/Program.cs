b7d91f7ab26b5b4a66f772fc55cc3c98cff64aa9
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-19 20:34:02 +0200

    Задача по разнице в количестве Москвичей и Петербужцев.

--- Rubinskiy/Program.cs
+++ Rubinskiy/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Moskvichi = 13015126;
+int Peterburgci = 5598486;
+int raznica = Moskvichi-Peterburgci;
+int otvet = raznica / 1000;
+Console.WriteLine($"Жителей Москвы на {raznica} тысяч больше, чем в Санкт-Петербурге");

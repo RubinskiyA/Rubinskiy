e963a90be8aca3fb0ef073ebeb286051eed36e2f
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-20 20:04:22 +0200

    Домашка 4-1-2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿//float d = 2.5f;
+//int i = (int)d;
+
+//Console.WriteLine(i);
+
+//Console.WriteLine((double)5/9);
+
+
+
+string myPerson = ("Александр\nРубинский");
+Console.WriteLine(myPerson);
+
+
+string myString = "284";
+Console.WriteLine(int.Parse(myString));
+
--- Rubinskiy/Program.cs
+++ Rubinskiy/Program.cs
@@ -1,23 +1,10 @@
-﻿// Тема 4 Урок 1-2 ( 
-//double Moskvichi = 13015126;
-//double Peterburgci = 5598486;
-//double raznica = Moskvichi - Peterburgci;
-//double otvet = raznica / 1000;
-//Console.WriteLine($"Жителей Москвы на {otvet} тысяч больше, чем в Санкт-Петербурге");
+﻿//Тема 4 Урок 1-2 ( 
+double Moskvichi = 13015126;
+double Peterburgci = 5598486;
+double raznica = Moskvichi - Peterburgci;
+double otvet = raznica / 1000;
+Console.WriteLine($"Жителей Москвы на {otvet} тысяч больше, чем в Санкт-Петербурге");
 
-//Тема 5 Урок 1 (Перевод миллиметров в сантиметры, метры, километры, мили и ярды)
-//Console.Write("Введите количество миллиметров:");
-//const double MilliToSanti = 10;
-//const double MilliToMetr = 1000;
-//const double MilliToKillo = 1000000;
-//const double MilliToMile = 1609344;
-//const double MilliToYards = 0.0010936100483;
-//double millimetrs = double.Parse(Console.ReadLine());
-//double santimetrs = millimetrs / MilliToSanti;
-//double meters = millimetrs / MilliToMetr;
-//double killometers = millimetrs / MilliToKillo;
-//double mile = millimetrs / MilliToMile;
-//double yards = millimetrs * MilliToYards;
-//Console.WriteLine($"Сантиметров:{santimetrs}\nМетров:{meters}\nКилометров:{killometers}\nМиль:{mile}\nЯрдов:{yards}");
-//Тема 5 Урок 2
-const string zagolovok = "№ Название        Цена    Кол.    Сумма";
+
+
+

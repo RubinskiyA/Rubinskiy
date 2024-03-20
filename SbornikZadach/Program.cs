a88bca1babae70cfb0e4c5ba1eb44ede6a3caa46
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-20 21:01:08 +0200

    Решение задач из Сборника задач

---
+++ SbornikZadach/Program.cs
@@ -0,0 +1,16 @@
+﻿//Console.WriteLine($"{Math.PI:F2}");
+//Console.WriteLine($"{Math.E:F1}");
+
+Console.Write("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double c = double.Parse(Console.ReadLine());
+Console.Write("Введите четвертое число: ");
+double d = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Вот ваши числа:\n"+$"{a}\n" +
+    $"{b}\n" +
+    $"{c} \n" +
+    $"{d}");

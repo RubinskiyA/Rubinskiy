696d1b68c7a5213b3c75dff429522bd43d1e8c08
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-20 20:38:22 +0200

    Классная работа

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -7,10 +7,25 @@
 
 
 
-string myPerson = ("Александр\nРубинский");
-Console.WriteLine(myPerson);
+//string myPerson = ("Александр\nРубинский");
+//Console.WriteLine(myPerson);
 
 
-string myString = "284";
-Console.WriteLine(int.Parse(myString));
+//string myString = "284";
+//Console.WriteLine(int.Parse(myString));
+
+
+using System.Diagnostics;
+
+byte bt=byte.Parse(Console.ReadLine());
+long freq = Stopwatch.Frequency;
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+//int integer = (int)bt;
+double doub = (double)bt;
+stopwatch.Stop();
+decimal sec = (decimal)stopwatch.ElapsedTicks/freq;
+Console.WriteLine($"bt={bt} \n Частота таймера " + $"{freq} такт/с \n Время в тактах {stopwatch.ElapsedTicks} \n Время в секундах " + $"{sec}");
+Console.WriteLine(doub);
+
 

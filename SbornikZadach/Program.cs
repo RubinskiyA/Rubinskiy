b30e473ca99621f0a31ac3af06ee9e1ffcb271d4
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-20 21:18:46 +0200

    классная работа

--- SbornikZadach/Program.cs
+++ SbornikZadach/Program.cs
@@ -1,16 +1,20 @@
 ﻿//Console.WriteLine($"{Math.PI:F2}");
 //Console.WriteLine($"{Math.E:F1}");
+//Задача 1.11
+//Console.Write("Введите первое число: ");
+//double a = double.Parse(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//double b = double.Parse(Console.ReadLine());
+//Console.Write("Введите третье число: ");
+//double c = double.Parse(Console.ReadLine());
+//Console.Write("Введите четвертое число: ");
+//double d = double.Parse(Console.ReadLine());
 
-Console.Write("Введите первое число: ");
-double a = double.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-double b = double.Parse(Console.ReadLine());
-Console.Write("Введите третье число: ");
-double c = double.Parse(Console.ReadLine());
-Console.Write("Введите четвертое число: ");
-double d = double.Parse(Console.ReadLine());
-
-Console.WriteLine($"Вот ваши числа:\n"+$"{a}\n" +
-    $"{b}\n" +
-    $"{c} \n" +
-    $"{d}");
+//Console.WriteLine($"Вот ваши числа:\n"+$"{a}\n" +
+//    $"{b}\n" +
+//    $"{c} \n" +
+//    $"{d}");
+int x = 5;
+int y = 6;
+Console.WriteLine(Math.Sin(60*Math.PI/180));
+Console.WriteLine(-5 * Math.Sqrt(x + Math.Sqrt(y)));

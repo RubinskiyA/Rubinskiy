4c9e3fc078ac65fc05c36c04563aca543774a69d
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-24 20:17:43 +0200

    Домашнее задание  4

---
+++ Lesson 4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 1
+double mskPeople = 13015126;
+double spbPeople = 5598486;
+double raznica = (mskPeople - spbPeople) / 1000;
+Console.WriteLine($"На {raznica} тысяч людей больше в Москве, чем в Санкт-Петербурге.");
+//Задача 2
+decimal plataYear = 260548.56895M;
+decimal plataMonth = plataYear / 12;
+decimal plata_NDFL = plataMonth * 0.87M;
+Console.WriteLine($"Работник получает {plataMonth:F2} рублей в месяц до вычета НДФЛ, с вычетом - {plata_NDFL:F2} рублей");
+

28ee4db8e6b7573eee5aeb72b00136ee6f50e37e
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-03-25 21:24:14 +0200

    Lesson7 Classwork

--- Lesson7Classwork/Program.cs
+++ Lesson7Classwork/Program.cs
@@ -16,51 +16,57 @@
 
 //Чек с вводимыми данными
 
-const string INN = "390001234567";
-const string COMP = "Бульбазавр и КО";
+//const string INN = "390001234567";
+//const string COMP = "Бульбазавр и КО";
 
-Console.Write("Введите название продукта:");
-string name1 = Console.ReadLine();
-Console.Write("Введите количество:");
-int count1 = int.Parse(Console.ReadLine());
-Console.Write("Введите цену:");
-decimal price1 = decimal.Parse(Console.ReadLine());
+//Console.Write("Введите название продукта:");
+//string name1 = Console.ReadLine();
+//Console.Write("Введите количество:");
+//int count1 = int.Parse(Console.ReadLine());
+//Console.Write("Введите цену:");
+//decimal price1 = decimal.Parse(Console.ReadLine());
 
-Console.Write("Введите название продукта:");
-string name2 = Console.ReadLine();
-Console.Write("Введите количество:");
-int count2 = int.Parse(Console.ReadLine());
-Console.Write("Введите цену:");
-decimal price2 = decimal.Parse(Console.ReadLine()); 
-Console.Write("Введите название продукта:");
+//Console.Write("Введите название продукта:");
+//string name2 = Console.ReadLine();
+//Console.Write("Введите количество:");
+//int count2 = int.Parse(Console.ReadLine());
+//Console.Write("Введите цену:");
+//decimal price2 = decimal.Parse(Console.ReadLine()); 
+//Console.Write("Введите название продукта:");
 
-string name3 = Console.ReadLine();
-Console.Write("Введите количество:");
-int count3 = int.Parse(Console.ReadLine());
-Console.Write("Введите цену:");
-decimal price3 = decimal.Parse(Console.ReadLine()); 
-Console.Write("Введите название продукта:");
+//string name3 = Console.ReadLine();
+//Console.Write("Введите количество:");
+//int count3 = int.Parse(Console.ReadLine());
+//Console.Write("Введите цену:");
+//decimal price3 = decimal.Parse(Console.ReadLine()); 
+//Console.Write("Введите название продукта:");
 
-string name4 = Console.ReadLine();
-Console.Write("Введите количество:");
-int count4 = int.Parse(Console.ReadLine());
-Console.Write("Введите цену:");
-decimal price4 = decimal.Parse(Console.ReadLine());
-decimal sum = count1*price1+count2*price2+count3*price3+count4*price4;
+//string name4 = Console.ReadLine();
+//Console.Write("Введите количество:");
+//int count4 = int.Parse(Console.ReadLine());
+//Console.Write("Введите цену:");
+//decimal price4 = decimal.Parse(Console.ReadLine());
+//decimal sum = count1*price1+count2*price2+count3*price3+count4*price4;
 
-Console.Title = "Чек";
-Console.WriteLine("--------------------------------\n" +
-    $"Организация {COMP}\n" +
-    $"ИНН {INN}\n" +
-    "*****************************\n" +
-($"{"N",-4}{"Название",-10}{"Цена",-6}{"Кол.",-6}{"Сумма",-6}\n") +
-($"{1,-4}{name1,-10}{price1,-6}{count1,-6}{(count1 * price1),-6}\n") +
-($"{2,-4}{name2,-10}{price2,-6}{count2,-6}{(count2 * price2),-6}\n") +
-($"{3,-4}{name3,-10}{price3,-6}{count3,-6}{(count3 * price3),-6}\n") +
-($"{4,-4}{name4,-10}{price4,-6}{count4,-6}{(count4 * price4),-6}\n") +
-($"{"Итог",-26}{sum}"));
+//Console.Title = "Чек";
+//Console.WriteLine("--------------------------------\n" +
+//    $"Организация {COMP}\n" +
+//    $"ИНН {INN}\n" +
+//    "*****************************\n" +
+//($"{"N",-4}{"Название",-10}{"Цена",-6}{"Кол.",-6}{"Сумма",-6}\n") +
+//($"{1,-4}{name1,-10}{price1,-6}{count1,-6}{(count1 * price1),-6}\n") +
+//($"{2,-4}{name2,-10}{price2,-6}{count2,-6}{(count2 * price2),-6}\n") +
+//($"{3,-4}{name3,-10}{price3,-6}{count3,-6}{(count3 * price3),-6}\n") +
+//($"{4,-4}{name4,-10}{price4,-6}{count4,-6}{(count4 * price4),-6}\n") +
+//($"{"Итог",-26}{sum}"));
 
+//Приветствие и возраст пользователя.
 
-
+//Console.Write("Введите свое ФИО:");
+//string fio = Console.ReadLine();
+//Console.Write("Введите дату рождения(yyyy.MM.dd):");
+//DateTime birthDay = DateTime.Parse(Console.ReadLine());
+//int age = DateTime.Now.Year-birthDay.Year;
+//Console.WriteLine($"Привет, {fio}. Ваш возраст {age} лет.");
 
 
--- SbornikZadach/Program.cs
+++ SbornikZadach/Program.cs
@@ -14,7 +14,47 @@
 //    $"{b}\n" +
 //    $"{c} \n" +
 //    $"{d}");
-int x = 5;
-int y = 6;
-Console.WriteLine(Math.Sin(60*Math.PI/180));
-Console.WriteLine(-5 * Math.Sqrt(x + Math.Sqrt(y)));
+//int x = 5;
+//int y = 6;
+//Console.WriteLine(Math.Sin(60*Math.PI/180));
+//Console.WriteLine(-5 * Math.Sqrt(x + Math.Sqrt(y)));
+
+
+//1.17
+//Console.WriteLine("Введите x1:");
+//double x1=double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите x2:");
+//double x2 = double.Parse(Console.ReadLine());
+//double y = Math.Sqrt(x1 * x1 + x2 * x2);
+//Console.WriteLine(y);
+
+
+
+//Console.Write("Введите R1:");
+//double r1 = double.Parse(Console.ReadLine());
+//Console.Write("Введите R2:");
+//double r2 = double.Parse(Console.ReadLine());
+
+//double y = 1 / r1 + 1 / r2;
+//Console.WriteLine($"Результат выражеия (1/{r1}+1/{r2}) равен {y:F3}");
+
+
+
+Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите угол c:");
+double с = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество секунд:");
+int sec = int.Parse(Console.ReadLine());
+int hour = sec / 3600;
+int minute = sec % 3600 /60 ;
+int second = sec % 60 ;
+Console.WriteLine();
+
+
+
+
+

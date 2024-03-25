084926f458fb66e40b78ab2fa283580447e342f7
SuperPC <SuperPC@SUPERPC>
2024-03-25 14:57:49 +0200

    домашнее задание 6_2

---
+++ Lesson6_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число для конвертации:");
+double number = double.Parse(Console.ReadLine());
+string numString = number.ToString();
+byte numByte = byte.Parse(numString);
+nint numNint = (nint)numByte;
+int numInt = (int)numNint;
+long numLong = (long)numInt;
+double numDouble = (double)numLong;
+
+Console.WriteLine($"{number} {number.GetType()}");
+Console.WriteLine($"{numString} {numString.GetType()}");
+Console.WriteLine($"{numByte} {numByte.GetType()}");
+Console.WriteLine($"{numNint} {numNint.GetType()}");
+Console.WriteLine($"{numInt} {numInt.GetType()}");
+Console.WriteLine($"{numLong} {numLong.GetType()}");
+Console.WriteLine($"{numDouble} {numDouble.GetType()}");
+
+
+
+
+
+

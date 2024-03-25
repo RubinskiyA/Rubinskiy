f5f06f886fa158bb2ad201d1d1e34c7ce3426bce
SuperPC <SuperPC@SUPERPC>
2024-03-25 14:37:18 +0200

    Домашнее задание 6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,6 @@
+﻿string number = "28";
+byte numByte = byte.Parse(number);
+nint numNint = (nint)numByte;
+int numInt = (int)numNint;
+long numLong = (long)numInt;
+double numDouble = (double)numLong;
